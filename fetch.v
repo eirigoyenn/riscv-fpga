fa52a23e3c9ca2f1e813b6aff578b6f8b02fe491
Dimas Bosch <dbosch@itba.edu.ar>
2023-06-11 00:53:35 -0300

    Simplifico fetch

--- fetch.v
+++ fetch.v
@@ -1,29 +1,22 @@
 module fetch
 (
- input[31:0] readPC,
- input clk,
- input nreset,
- input ena,
- output [31:0] writePC,
- output[31:0] memAddress
+ input wire[31:0] PC_in,
+ input wire clk,
+ input wire ena,
+ output wire [31:0] PC_out,
+ output wire [31:0] memAddress
  );
 
-	reg[31:0] q, address;
-	wire reset;
+	reg[31:0] q;
 	
-	assign reset = ~nreset;
- 
-	always @(posedge clk, posedge reset) begin
-		if (reset) begin
-			q <= 0;	//Al reset, vuelve el PC a cero
-		end
-		else if (clk && ena) begin
-			q <= readPC + 4;	//Le suma 4 a cada pulso de clk
+	always @(posedge clk) begin
+		if (ena) begin
+			q <= PC_in + 4;	//Le suma 4 a cada pulso de clk
 		end
 	end
 	
-	assign memAddress = readPC;
-	assign writePC = q;
+	assign memAddress = PC_in;
+	assign PC_out = q;
 
 
 endmodule
