c6367933a7d996e8ba7ac8c6c4bae12ec97bdde3
Dimas Bosch <dbosch@itba.edu.ar>
2023-06-10 23:03:54 -0300

    RegBank 100% combinacional

--- reg_bank.v
+++ reg_bank.v
@@ -1,22 +1,23 @@
 module reg_bank(
-  input wire clk,
   input wire [4:0] readPort1, readPort2, writePort,
   input wire [31:0] busC,
   input wire regWrite,
-  output reg [31:0] busA, busB
+  output wire [31:0] busA, busB
 );
 
   reg [31:0] registers [31:0];  // Declaración de los registros
   initial begin
-    registers[0] = 32'h00000000;
+    registers[0] = 32'h00000000;  // Inicialización de registro 0 32'h00000000
+    
   end
-  always @(posedge clk) begin
-    if (regWrite && writePort) begin	//Si quiero escribir (no se puede escribir el reg 0)
-      registers[writePort] <= busC;  // Escritura en el registro seleccionado
-    end else begin
-		 busA <= registers[readPort1];  // Lectura del primer registro seleccionado
-		 busB <= registers[readPort2];  // Lectura del segundo registro seleccionado
-	 end
+
+  assign busA = registers[readPort1];  // Lectura del primer registro seleccionado
+  assign busB = registers[readPort2];  // Lectura del segundo registro seleccionado
+
+  always @(*) begin
+    if (regWrite && (writePort != 5'b00000)) begin  // Si quiero escribir (no se puede escribir el registro 0)
+      registers[writePort] = busC;  // Escritura en el registro seleccionado
+    end
   end
 
 endmodule
