59d900545e27c9273c1a4500e66b2c5a955c3329
Dimas Bosch <dbosch@itba.edu.ar>
2023-06-13 20:30:51 -0300

    arreglando cagadas

--- alu.v
+++ alu.v
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-module alu
-(
- input wire [31:0] busA,
- input wire [31:0] busB,
- input wire [2:0] funct3,
- input wire [6:0] funct7,
- input wire [6:0] op,
- output reg [31:0] busC
- );
- 
-	initial begin
-		 busC = 32'h00000000;  // Inicialización de registro 0 32'h00000000
-		 
-	end
-	 
-	always @(*)
-	begin
-		case (op)
-			7'b0000011:  busC = busA; // le pongo busA momentaneo para chequear funcionamiento
-			7'b0000111:  busC = 1;
-			7'b0001011:  busC = busA + busB;
-			7'b0001111:  busC = busA - busB;
-			7'b0010011:  busC = busA + 1;
-			7'b0010111:  busC = busA - 1;
-			7'b0100011:  busC = busA;		
-			7'b0100111:  busC = busB;		
-			default :    busC = 0;
-		endcase 
-	end
-=======
 `include "util.v"
 
 module alu (
@@ -48,7 +17,6 @@ module alu (
     reg [6:0] funct7;
     reg [31:0] abs_aux1, abs_aux2, new_PC;
     reg flag = 0;
->>>>>>> fa7639bcfa985d53d744e3fa08c2844f7d0905ec
 
     always @(*)
     begin
--- decoder.v
+++ decoder.v
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-module Decoder ( inst ,rs1,rs2,rd,funct3,funct7,imm_out,op);	
-
- input wire [31:0] inst;
- output reg [4:0] rs1;
- output reg [4:0] rs2; 
- output reg [4:0] rd; 
- output reg [2:0] funct3; //control de la alu
- output reg [6:0] funct7;
- output reg [6:0] op;
- output reg [11:0] imm_out;	//Saco 20 bits , a veces uso menos.
- 
- always @(inst)
- 
- begin 
-	case (inst[6:0]) //falta  nop => addi rs1=0 y rd=0
-		//Memory access , LOAD
-		7'b0000011: begin
-							 rs1=inst[19:15];		//entre A y immout forman el efective address =>17bits,lugar a donde ir a buscar un dato
-							 imm_out=inst[31:20];
-							 rd=inst[11:7];			//regitro al que se le carga un dato de la memoria
-							 funct3=inst[14:12];		//ojo que esto no iria a la alu, xq indica el ancho de la palabra
-							 rs2 = 5'b00000;
-							end	// de memo carga en rd
-		
-		//Interger RegisterImmediate Instructinon
-		7'b0010011: begin  							
-							 rs1=inst[19:15];
-							 rd=inst[11:7];		
-							 funct3=inst[14:12];
-							 imm_out=inst[31:20];
-							 rs2=5'b00000;
-							end  	
-				  
-		//Memory Access , STORE
-		7'b0100011: begin 
-							 rd=inst[11:7];	//usados para indicar el efective address =>17bits, lugar a donde ir a guardar
-							 rs1=inst[19:15];		
-							 imm_out=inst[31:25];
-							 rs2=inst[24:20];	 	//registro del cual se va a guarda el dato que posee en memoria
-							 funct3=inst[14:12];		//usados pa distinguir el largo del dato
-							 
-							end	//de rs2=> memo		
-		
-		//Integer Register-Register Operations
-		7'b0110011: begin //creo que no hace falta
-							 rs1=inst[19:15];
-							 rs2=inst[24:20];
-							 rd=inst[11:7];
-							 funct3=inst[14:12];		
-							 imm_out=inst[31:25];
-							end 
-=======
 //PRIMERA IMPLEMENTACION DECODER PARA RISC V 32I
 // DECODER
 module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como afecta si es litle/big endian
@@ -259,18 +206,12 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 						end	//de rs2=> memo		
 		
 
->>>>>>> fa7639bcfa985d53d744e3fa08c2844f7d0905ec
 		default: begin				//en caso de que pase algo , mando una instruccion NOP
 						rd=5'b00000;
 						funct3=3'b000;
 						rs1=5'b00000;
-<<<<<<< HEAD
-						imm_out=12'b000000000000;
-						rs2 = 5'b00000;
-=======
 						imm_out={25'b0000000000000000000000000,instruccion[31:25]};
 						opcode=7'b0110011;
->>>>>>> fa7639bcfa985d53d744e3fa08c2844f7d0905ec
 					end
 	endcase
 	op = inst[6:0];
