359caafe87be3d760d1a7d3424a158534335ceb6
jucardozo <jucardozo@itba.edu.ar>
2023-06-16 21:03:40 -0300

    se agrego falag , llamado ras, para manejo de los turnos del jmp control y ras

--- decoder.v
+++ decoder.v
@@ -1,15 +1,17 @@
 //PRIMERA IMPLEMENTACION DECODER PARA RISC V 32I
 // DECODER
-module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como afecta si es litle/big endian
+module Decoder ( instruccion,ras ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como afecta si es litle/big endian
 //input
  input[31:0] instruccion;
  //output
+ output reg ras;
  output reg [4:0] rs1;//rs1;
  output reg [4:0] rs2; //rs2
  output reg [4:0] rd; //rd
  output reg [2:0] funct3; //control de la alu
- output reg [31:0] imm_out;	// IMM,a chequear ... no se bien cuantos bits tienen que salir
+ output reg [31:0] imm_out;	
  output reg[6:0] opcode;
+ reg ras_flag=0;
  always @(instruccion) // creo que aca tendria que poner el flanco de algun clock
 	 begin 
 		case (instruccion[6:0]) //falta  nop => addi rs1=0 y rd=0
@@ -20,6 +22,7 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 								 rd=instruccion[11:7];		
 								 funct3=instruccion[14:12];
 								 opcode=7'b0010011;
+								 ras=0;
 								 case (instruccion[14:12])
 								  3'b000:begin
 												if(instruccion[31]==1'b1)
@@ -77,9 +80,11 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 								rs1=0;
 								rs2=0;
 								funct3=0;
+								ras<=0;
 								opcode=7'b0110111; //grupo de instruccion
 							end
 			7'b0010111:begin		//AUIPC?
+								ras<=0;
 								rd=instruccion[11:7];
 								imm_out={instruccion[31:12],12'b000000000000};
 								rs1=0;
@@ -89,6 +94,7 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 							end
 			//Integer Register-Register Operations
 			7'b0110011: begin //creo que no hace falta
+								 ras<=0;
 								 rs1=instruccion[19:15];
 								 rs2=instruccion[24:20];
 								 rd=instruccion[11:7];
@@ -103,11 +109,20 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 								rs2=0;
 								funct3=0;
 								opcode=7'b1101111;
+								if((rd==5'b00001)|(rd==5'b00101))
+								begin
+								ras=1;
+								end
+								else
+								begin 
+								ras=0;
+								end
 								if(instruccion[31]==1'b1)
 								begin //veo signo
 									imm_out={11'b11111111111,instruccion[31],instruccion[19:12],instruccion[20],instruccion[30:21],1'b0};
 								end
 								else begin
+									//imm_out=rd+(32'h00000001);
 									imm_out={11'b00000000000,instruccion[31],instruccion[19:12],instruccion[20],instruccion[30:21],1'b0};
 								end
 							end
@@ -117,6 +132,14 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 								rs2=0;
 								funct3=0;
 								opcode=7'b1100111;
+								if(((rd==5'b00001)|(rd==5'b00101))|((rs1==5'b00001)|(rs1==5'b00101))) 
+								begin
+								ras=1;
+								end
+								else
+								begin
+								ras=0;
+								end
 								if(instruccion[31]==1'b1)
 								begin //veo signo
 									imm_out={20'hFFFFF,instruccion[31:20]};
@@ -130,6 +153,7 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 								rs1=instruccion[19:15];
 								rs2=instruccion[24:20];
 								rd=0;
+								ras=0;
 								opcode=7'b1100011;
 								case (instruccion[14:12])
 									3'b000:begin
@@ -181,6 +205,7 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 								 funct3=instruccion[14:12];		//ojo que esto no iria a la alu, xq indica el ancho de la palabra
 								 opcode=7'b0000011;
 								 rs2=0;
+								 ras=0;
 								 if(instruccion[31]==1'b1)
 								 begin //veo signo
 									imm_out={20'hFFFFF,instruccion[31:20]};
@@ -192,6 +217,7 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 			
 			//Memory Access , STORE
 			7'b0100011: begin 
+								 ras=0;
 								 rd=instruccion[11:7];	//usados para indicar el efective address =>17bits, lugar a donde ir a guardar
 								 rs1=instruccion[19:15];		
 								 rs2=instruccion[24:20];	 	//registro del cual se va a guarda el dato que posee en memoria
@@ -212,6 +238,7 @@ module Decoder ( instruccion ,rs1,rs2,rd,funct3,imm_out,opcode);	//PEnsar como a
 							funct3=3'b000;
 							rs1=5'b00000;
 							rs2=0;
+							ras=0;
 							imm_out={25'b0000000000000000000000000,instruccion[31:25]};
 							opcode=7'b0110011;
 						end
