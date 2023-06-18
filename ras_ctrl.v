801664932e9b3fd623af6fd844e5cedf2f38c975
jucardozo <jucardozo@itba.edu.ar>
2023-06-17 21:16:21 -0300

    se agrega ras_ctrl testeado ,falta ras_stack y arreglar flags

--- ras_ctrl.v
+++ ras_ctrl.v
@@ -16,11 +16,11 @@
 	           pop
 				  pctoras
 */
-module ras_ctrl (opcode,reset_in,imm_in,rs1,rd,pc,pcfromras,ras_en,pc_jmp,reset_out,push,pop,pctoras);
+module ras_ctrl (opcode,reset_in,imm_in,rs1,rd,pc_in,pcfromras,ras_en,pc_jmp,reset_out,push,pop,pctoras);
 //INPUTS
 input wire[6:0] opcode;
 input wire[31:0] imm_in;
-input wire[31:0] pc;
+input wire[31:0] pc_in;
 input wire[4:0] rs1;
 input wire[4:0] rd;
 input wire ras_en;
@@ -36,40 +36,39 @@ output wire [31:0] pc_jmp; //Direccion a donde saltar.
 
 reg [31:0] pc_to_ras;
 reg [31:0] pc_jmp_;
+reg [31:0] aux=32'h00000000;
 reg push_flag;
 reg pop_flag;
 reg reset;
 
 always @(posedge ras_en)
 begin 
-	if(ras_en)
-	begin
-		if (reset_in) 
-			begin 
-				reset=1;
-				pop_flag=0;
-				push_flag=0;
-				pc_to_ras=32'hFFFFFFFF;
-				pc_jmp_=pc+(32'h00000004);
-			end
-		else
-			begin
+	if (reset_in) 
+		begin 
+			reset=1'b1;
+			pop_flag=1'b0;
+			push_flag=1'b0;
+			pc_to_ras=32'hFFFFFFFF;
+			pc_jmp_=pc_in+(32'h00000004);
+		end
+	else
+		begin
 				case(opcode)
 					7'b1101111://JAL el decoder me pasa si uso los registro x1 o x5
 						begin
 							if(imm_in!=0)	//Logica de Push , solo si se usan los registros x1=1 y x5=5(por documentacion)
 							begin
-								pc_to_ras = pc+(32'h00000004); //guardo la direccion de retorno.
-								push_flag=1;
-								pop_flag=0;
-								pc_jmp_=(pc+imm_in);			//mando al pc la nueva direccion.	
-								reset=0;
+								pc_to_ras = pc_in+(32'h00000004); //guardo la direccion de retorno.
+								push_flag=1'b1;
+								pop_flag=1'b0;
+								pc_jmp_=(pc_in+imm_in);			//mando al pc la nueva direccion.	
+								reset=1'b0;
 							end
 							else			// Logica de Pop , imm_in=0; 
 							begin			
-								pop_flag=1;
-								push_flag=0;
-								reset=0;
+								pop_flag=1'b1;
+								push_flag=1'b0;
+								reset=1'b0;
 								pc_jmp_=pcfromras;
 								pc_to_ras=32'hFFFFFFFF;
 							end	
@@ -79,63 +78,63 @@ begin
 						begin	
 							if(((rs1==5'h01)||(rs1==5'h05))&&((rd!=5'h01)||(rd!=5'h05))) //pop
 							begin
-								pop_flag=1;
-								push_flag=0;
-								reset=0;
+								pop_flag=1'b1;
+								push_flag=1'b0;
+								reset=1'b0;
 								pc_jmp_=pcfromras;
 								pc_to_ras=32'hFFFFFFFF;
 							end
 							
 							else if(((rd==5'h01)||(rd==5'h05))&&((rs1!=5'h01)||(rs1!=5'h05))) //push 
 							begin
-								pc_to_ras= pc+(32'h00000004); 
-								push_flag=1;
-								pop_flag=0;
-								pc_jmp_=(pc+imm_in);				//VER BIEN COMO SE SUMA
-								reset=0;
+								pc_to_ras= pc_in+(32'h00000004); 
+								push_flag=1'b1;
+								pop_flag=1'b0;
+								aux=(imm_in+rs1);
+								pc_jmp_=(pc_in+{aux[31:1],1'b0});				//VER BIEN COMO SE SUMA
+								reset=1'b0;
 							end
 							 
 							else if(((rs1==5'h01)||(rs1==5'h05))&&((rd!=5'h01)||(rd!=5'h05))) 
 							begin	
 								if(rd==rs1)	//Push
 								begin
-									pc_to_ras = pc+(32'h00000004); 
-									push_flag=1;
-									pop_flag=0;
-									pc_jmp_=(pc+imm_in);				
-									reset=0;
+									pc_to_ras = pc_in+(32'h00000004); 
+									push_flag=1'b1;
+									pop_flag=1'b0;
+									pc_jmp_=(pc_in+{aux[31:1],1'b0});				
+									reset=1'b0;
 								end
 									
 								else		//Pop , y push ,creo que en ese orden //NO ENTIENDO QUE HACE TODO
 								begin	
-									pop_flag=1;
-									push_flag=0;
-									reset=0;
+									pop_flag=1'b1;
+									push_flag=1'b0;
+									reset=1'b0;
 									pc_jmp_=pcfromras;
-									pc_to_ras = pc+(32'h00000004); 
+									pc_to_ras = pc_in+(32'h00000004); 
 								end
 								
 							end
 							else
 							begin
-								pop_flag=0;
-								push_flag=0;
-								reset=0;
+								pop_flag=1'b0;
+								push_flag=1'b0;
+								reset=1'b0;
 								pc_to_ras=32'hFFFFFFFF;
-								pc_jmp_=pc+(32'h00000004);
+								pc_jmp_=pc_in+(32'h00000004);
 							end
 						end
 					default:
 						begin
-							pop_flag=0;
-							push_flag=0;
-							reset=0;
+							pop_flag=1'b0;
+							push_flag=1'b0;
+							reset=1'b0;
 							pc_to_ras=32'hFFFFFFFF;
-							pc_jmp_=pc+(32'h00000004);
+							pc_jmp_=pc_in+(32'h00000004);
 						end
 				endcase
 			end
-	end
 end
 assign pctoras=pc_to_ras;
 assign pc_jmp=pc_jmp_;
--- ras_stack.v
+++ ras_stack.v
@@ -4,23 +4,28 @@
 			  pop
 			  pc_in(return)
   OUTPUTS => pc_out()
-			    sel_mux
+			    
 */
-module ras_stack (input wire reset,input wire push,input wire pop,input wire [31:0] pc_in,output reg [31:0] pc_out);
+module ras_stack (reset, push, pop, pc_in, pc_out);
 
+  input wire reset;
+  input wire push;
+  input wire pop;
+  input wire[31:0] pc_in;
+  
+  output wire [31:0] pc_out;
+  
   reg [31:0] stack [0:31];
   reg [4:0] tos;
-  wire [2:0] next_tos;
-  wire [7:0] next_data_out;
-  //wire next_empty;
-  //wire next_full;
+  reg [31:0] pc_out_;
+
 
   always @(*)
   begin
     if (reset)
 	 begin
       tos = 0;
-		pc_out=32'hFFFFFFFF;
+		pc_out_=32'hFFFFFFFF;
      
     end 
 	 else 
@@ -28,23 +33,17 @@ module ras_stack (input wire reset,input wire push,input wire pop,input wire [31
 		if(push)
 		begin
 			stack[tos]=pc_in;
-			pc_out=stack[tos];
+			pc_out_=stack[tos];
 		end
 		else if(pop)
 		begin
-			pc_out=stack[tos-5'b00001];
+			pc_out_=stack[tos-5'b00001];
 		end
 		tos=(push ? tos + 5'b00001 : (pop && tos > 0 ? tos - 5'b00001 : tos));
 		//tos = next_tos;
     end
   end
-
- // assign next_tos = push ? top + 1 : (pop && top > 0 ? top - 1 : top);
-  //assign next_pc_out = stack[tos-1];
-  //assign pc_out = next_data_out;
-  //assign next_empty = (top == 0);
-  //assign next_full = (top == 7);
-  //assign empty = next_empty;
-  //assign full = next_full;
+  
+assign pc_out=pc_out_;
 
 endmodule
